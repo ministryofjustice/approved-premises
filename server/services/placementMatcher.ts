e6aac90e102c9138fe0f564ebf75753607043881
pezholio <pezholio@gmail.com>
2022-05-12 09:32:53 +0100

    Show available beds in the search results
    This is a bit hacky as we need to do an `array_agg` to get things
    like gender, and lift_or_stairlift (which should be attributes of the
    premises), but the meat of the query is where we do a count on
    beds and filter by their availability.

--- server/services/placementMatcher.ts
+++ server/services/placementMatcher.ts
@@ -18,10 +18,8 @@ export default class PlacementMatcher {
 
   public async results(): Promise<any[]> {
     const { indexName } = config.opensearch
-    const respository = console.log(
-      'Finding suitable, available, nearby candidate placements near',
-      this.filterArgs.location
-    )
+
+    console.log('Finding suitable, available, nearby candidate placements near', this.filterArgs.location)
 
     const lat = 53.7901124
     const lon = -1.560001909
@@ -52,15 +50,11 @@ export default class PlacementMatcher {
     const searchResults = response.body.hits
     const ids = searchResults.hits.map((hit: any) => hit._source.premises.id)
 
-    const premises = await this.repository
-      .createQueryBuilder('premises')
-      .leftJoinAndSelect('premises.beds', 'beds')
-      .where('premises.id IN(:...ids)', { ids: ids })
-      .getMany()
+    const premises = await this.fetchPremisesByIds(ids)
 
-    const sortedPremisesWithMetadata = premises
+    const results = premises
       .map(p => {
-        const searchResult = searchResults.hits.find((r: any) => r._source.premises.id === p.id)
+        const searchResult = searchResults.hits.find((r: any) => r._source.premises.id === p.premises_id)
 
         return {
           ...p,
@@ -70,7 +64,41 @@ export default class PlacementMatcher {
       })
       .sort((a, b) => b.score - a.score)
 
-    return sortedPremisesWithMetadata
+    return results
+  }
+
+  private async fetchPremisesByIds(ids: Array<string>): Promise<any[]> {
+    let query = this.repository
+      .createQueryBuilder('premises')
+      .distinctOn(['premises.id'])
+      .select(['premises'])
+      .addSelect(['array_agg("beds"."gender") AS gender'])
+      .addSelect(['array_agg("beds"."enhanced_security") AS enhanced_security'])
+      .addSelect(['array_agg("beds"."lift_or_stairlift") AS lift_or_stairlift'])
+      .addSelect(['array_agg("beds"."step_free_access_to_communal_areas") AS step_free_access_to_communal_areas'])
+      .addSelect(['array_agg("beds"."iap") AS iap'])
+      .addSelect(['array_agg("beds"."pipe") AS pipe'])
+      .leftJoin('premises.beds', 'beds')
+      .leftJoin('beds.bookings', 'bookings')
+      .where('premises.id IN(:...ids)', { ids: ids })
+      .groupBy('premises.id')
+
+    if (this.filterArgs.gender) {
+      query = query.andWhere('gender = :gender', { gender: this.filterArgs.gender })
+    }
+
+    if (this.filterArgs.date_from) {
+      query = query.addSelect(
+        `COUNT(beds.id) filter (
+    WHERE
+      bookings IS NULL
+      OR NOT (DATE '${this.filterArgs.date_from.toDateString()}', DATE '${this.filterArgs.date_to.toDateString()}') OVERLAPS (bookings.start_time, bookings.end_time)
+  )`,
+        'bed_count'
+      )
+    }
+
+    return await query.getRawMany()
   }
 
   private query(lat: number, lon: number) {
