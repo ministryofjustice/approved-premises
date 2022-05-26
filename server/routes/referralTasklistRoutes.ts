042a9668097422228615afcf3826ebf2117a2a23
Ed Davey <ed.davey@digital.justice.gov.uk>
2022-05-26 08:49:22 +0100

    Fix: risk info sidepanel now populated again on Tasklist

--- server/routes/referralTasklistRoutes.ts
+++ server/routes/referralTasklistRoutes.ts
@@ -10,21 +10,19 @@ export const referralTasklistUrlPrefix = '/referral_tasklist'
 export default function ReferralTasklistRoutes(router: Router): Router {
   get(router, '/', async (req, res, next) => {
     const risks = {
-      risks: {
-        mappa: {
-          level: 'CAT 2/LEVEL 1',
-          isNominal: false,
-          lastUpdated: '10th October 2021',
-        },
-        flags: ['Hate Crime'],
-        roshRiskSummary: {
-          overallRisk: 'VERY_HIGH',
-          riskToChildren: 'LOW',
-          riskToPublic: 'VERY_HIGH',
-          riskToKnownAdult: 'MEDIUM',
-          riskToStaff: 'HIGH',
-          lastUpdated: '10th October 2021',
-        },
+      mappa: {
+        level: 'CAT 2/LEVEL 1',
+        isNominal: false,
+        lastUpdated: '10th October 2021',
+      },
+      flags: ['Hate Crime'],
+      roshRiskSummary: {
+        overallRisk: 'VERY_HIGH',
+        riskToChildren: 'LOW',
+        riskToPublic: 'VERY_HIGH',
+        riskToKnownAdult: 'MEDIUM',
+        riskToStaff: 'HIGH',
+        lastUpdated: '10th October 2021',
       },
     }
 
