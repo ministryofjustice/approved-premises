7bdfdb1d4555dc729899e3cd46d00358304dd788
pezholio <pezholio@gmail.com>
2022-05-31 16:06:45 +0100

    Add Questions to the CCTV agency step

--- server/forms/questions/question.spec.ts
+++ server/forms/questions/question.spec.ts
@@ -41,5 +41,76 @@ describe('Question', () => {
       const html = await question.present()
       expect(html).toContain('<span class="govuk-visually-hidden">Error:</span> You must specify a type')
     })
+  describe('with checkboxes', () => {
+    it('renders a group of checkboxes', async () => {
+      const step = createMock<ApTypeStep>()
+
+      const question = new Question(step, 'cctv-reasons')
+
+      const html = await question.present()
+
+      expect(html).toContain(
+        'Which behaviours has Robert demonstrated that require enhanced CCTV provision to monitor?'
+      )
+    })
+
+    it('prepopulates an option if already selected', async () => {
+      const step = createMock<ApTypeStep>()
+
+      step.body.cctvReasons = ['appearance', 'networks', 'community-threats']
+
+      const question = new Question(step, 'cctv-reasons')
+
+      const html = await question.present()
+
+      expect(html).toContain(
+        '<input class="govuk-checkboxes__input" id="cctvReasons" name="cctvReasons[]" type="checkbox" value="appearance" checked>'
+      )
+
+      expect(html).toContain(
+        '<input class="govuk-checkboxes__input" id="cctvReasons-2" name="cctvReasons[]" type="checkbox" value="networks" checked>'
+      )
+
+      expect(html).toContain(
+        '<input class="govuk-checkboxes__input" id="cctvReasons-6" name="cctvReasons[]" type="checkbox" value="community-threats" checked>'
+      )
+    })
+
+    it('adds the error messages to the group', async () => {
+      const step = createMock<ApTypeStep>()
+
+      step.errors = {
+        cctvReasons: ['You must specify a reason'],
+      }
+
+      const question = new Question(step, 'cctv-reasons')
+
+      const html = await question.present()
+      expect(html).toContain('<span class="govuk-visually-hidden">Error:</span> You must specify a reason')
+    })
+  })
+
+  describe('with a textarea', () => {
+    it('returns a textarea', async () => {
+      const step = createMock<ApTypeStep>()
+
+      const question = new Question(step, 'cctv-supporting-information')
+
+      const html = await question.present()
+
+      expect(html).toContain('Provide any supporting information about why Robert requires enhanced CCTV provision.')
+    })
+
+    it('prepopulates the question', async () => {
+      const step = createMock<ApTypeStep>()
+
+      step.body.cctvSupportingInformation = 'text goes here'
+
+      const question = new Question(step, 'cctv-supporting-information')
+
+      const html = await question.present()
+
+      expect(html).toContain('text goes here')
+    })
   })
 })
--- server/forms/steps/cctv.step.ts
+++ server/forms/steps/cctv.step.ts
@@ -1,8 +1,8 @@
 import { plainToInstance } from 'class-transformer'
 
 import CCTV from '../dtos/cctv'
-
 import Step from './step'
+import Question from '../questions/question'
 
 export default class CCTVStep extends Step {
   section = 'ap-type' as const
@@ -28,5 +28,13 @@ export default class CCTVStep extends Step {
 
   allowedToAccess(): boolean {
     return this.sessionData?.esapReasons?.includes('cctv') || false
+  }
+
+  questions(): Array<Question> {
+    return [
+      new Question(this, 'cctv-reasons'),
+      new Question(this, 'cctv-agency-request'),
+      new Question(this, 'cctv-supporting-information'),
+    ]
   }
 }
