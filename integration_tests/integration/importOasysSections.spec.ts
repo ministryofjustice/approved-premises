cc69b7ac9738438aa58e840835ee5a7cd2d06399
rich <rich.james@dxw.com>
2022-06-14 11:50:44 +0100

    Create page object and make a start on e2e tests
    The tests don't add a tonne of value at the moment but it's worth making a start for them to be added to later
    
    And they allow us to assert the checkboxes and CTA exist

---
+++ integration_tests/integration/importOasysSections.spec.ts
@@ -0,0 +1,25 @@
+import Page from '../pages/page'
+import OasysImportRisks from '../pages/importOasysSections'
+
+context('SignIn', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubAuthUser')
+    cy.exec('npm run seed:premises')
+  })
+
+  it('checkboxes can be toggled', () => {
+    cy.signIn()
+
+    // Given I am on the Oasys risks import page
+    cy.visit('/risks/summary')
+    const ImportOasysRisksPage = Page.verifyOnPage(OasysImportRisks)
+
+    // When I toggle all the checkboxes
+    ImportOasysRisksPage.toggleAllSections()
+
+    // Then I can click save and continue
+    ImportOasysRisksPage.saveAndContinue()
+  })
+})
